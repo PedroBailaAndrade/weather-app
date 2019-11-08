45743b7e7262ed440f821c3c3156b089bc7be496
Pedro Andrade <pedrocbva@gmail.com>
2019-11-08 16:45:56 +0000

    api

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
 window.addEventListener("load", () => {
   let long;
   let lat;
+  let locationName = document.querySelector(".location-name")
+  let temperatureDescription = document.querySelector(".temperature-discription")
+  let temperatureDegree = document.querySelector(".temperature-degree")
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
-
-      const api = `https://api.darksky.net/forecast/0bd98fcf5467b3790354289f42410edc/${lat},${long}
-`;
-      fetch(api)
+      const key = `522eb3708f95c8b0b1700def0755b23a`;
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${key}&units=metric`;
+      fetch(url)
         .then(response => {
           return response.json();
         })
         .then(data => {
           console.log(data);
-        });
+          const name = data.name;
+          const {temp, humidity} = data.main;
+          const {main, description, icon} = data.weather["0"];
 
+          temperatureDegree.textContent = temp;
+          temperatureDescription.textContent = description;
+          locationName.textContent = name;
+        });
     });
   }
 });
